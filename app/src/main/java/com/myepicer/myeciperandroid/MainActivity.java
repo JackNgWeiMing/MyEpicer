770bc408fbcd0c534c3148ebff98afaff98d676b
JackNgWeiMing <jackng9995@gmail.com>
2017-06-15 20:50:04 +0800

    Implemeted ListView Add Recipe Class

--- app/src/main/java/com/myepicer/myeciperandroid/MainActivity.java
+++ app/src/main/java/com/myepicer/myeciperandroid/MainActivity.java
@@ -1,6 +1,10 @@
 package com.myepicer.myeciperandroid;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,9 +12,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.myepicer.myeciperandroid.Model.Recipe;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private List<Recipe> recipeList = Recipe.getListInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        ListView recipeListView = (ListView) findViewById(R.id.recipeListView);
+        recipeListView.setAdapter(new RecipeAdapter());
+
     }
 
     @Override
@@ -48,5 +66,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+
+    private  class RecipeAdapter extends ArrayAdapter<Recipe>{
+
+        public RecipeAdapter() {
+            super(MainActivity.this, R.layout.listview_item_recipe,recipeList);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            if(convertView==null){
+                convertView = getLayoutInflater().inflate(R.layout.listview_item_recipe,parent,false);
+            }
+
+            TextView txtRecipeTitle,txtLevel,txtTime,txtCost;
+            ImageView imgRecipe ;
+            Recipe recipe = recipeList.get(position);
+
+            imgRecipe = (ImageView) convertView.findViewById(R.id.imgRecipe);
+            txtRecipeTitle = (TextView) convertView.findViewById(R.id.txtRecipeTitle);
+            txtLevel = (TextView) convertView.findViewById(R.id.txtLevel);
+            txtTime = (TextView) convertView.findViewById(R.id.txtTime);
+            txtCost = (TextView) convertView.findViewById(R.id.txtCost);
+
+            imgRecipe.setImageResource(recipe.getPicture());
+            txtRecipeTitle.setText(recipe.getName());
+            txtLevel.setText(recipe.getLevel());
+            txtTime.setText(recipe.getTime());
+            txtCost.setText(String.valueOf(recipe.getCost()));
+
+            return convertView;
+        }
     }
 }
--- app/src/main/java/com/myepicer/myeciperandroid/Model/Recipe.java
+++ app/src/main/java/com/myepicer/myeciperandroid/Model/Recipe.java
@@ -1,9 +1,63 @@
 package com.myepicer.myeciperandroid.Model;
 
+import com.myepicer.myeciperandroid.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Acer on 15/6/2017.
  */
 
 public class Recipe {
-  
+
+    private String name;
+    private String level;
+    private String time;
+    private double cost;
+    private int picture;
+
+    public Recipe(String name, String level, String time, double cost) {
+        this.name = name;
+        this.level = level;
+        this.time = time;
+        this.cost = cost;
+    }
+
+    public Recipe(String name, String level, String time, double cost, int picture) {
+        this(name, level,time,cost);
+        this.picture = picture;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public int getPicture() {
+        return picture;
+    }
+
+    public static Recipe getInstance(){
+        return new Recipe("Transditinal Steamed Eggs","Beg","30min",10);
+    }
+
+    public static List<Recipe> getListInstance(){
+        List<Recipe> recipeList = new ArrayList<>();
+        recipeList.add(new Recipe("Transditinal Steamed Eggs","Beg","30min",10, R.drawable.chinese_steamed_egg));
+        recipeList.add(new Recipe("Grandma Steamed Egg","Beg","30min",10,R.drawable.chinese_steamed_egg2));
+        recipeList.add(new Recipe("Special garden steamed eggs","Beg","30min",10,R.drawable.tranditional_steamed_egg2));
+        return recipeList;
+    }
 }
