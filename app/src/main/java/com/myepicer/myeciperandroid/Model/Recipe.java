1294f83c38d6e86782b3c4f06a0518ed3c9a50db
JackNgWeiMing <jackng9995@gmail.com>
2017-06-15 19:40:32 +0800

    Initial commit

---
+++ app/src/main/java/com/myepicer/myeciperandroid/Model/Recipe.java
@@ -0,0 +1,9 @@
+package com.myepicer.myeciperandroid.Model;
+
+/**
+ * Created by Acer on 15/6/2017.
+ */
+
+public class Recipe {
+  
+}
---
+++ app/src/main/java/com/myepicer/myeciperandroid/data/SampleRecipe.java
@@ -0,0 +1,8 @@
+package com.myepicer.myeciperandroid.data;
+
+/**
+ * Created by Acer on 15/6/2017.
+ */
+
+public class SampleRecipe {
+}
